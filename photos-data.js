d237fd64920dfb876e6d1d76cc1c07a7665709ce
Randall Schirra <77857060+schiracha@users.noreply.github.com>
2026-07-09 21:49:39 -0400

    boomboom

--- photos-data.js
+++ photos-data.js
@@ -14,7 +14,7 @@
 const REPO_OWNER = 'schiracha';
 const REPO_NAME = 'cliffelab';
 const REPO_BRANCH = 'main';
-const PHOTO_PATH = 'LAB PHOTOS';
+const PHOTO_PATH = 'Lab Photos';
 
 function parseDateFromName(name) {
   const m = name.match(/-(\d{4})(\d{2})(\d{2})\.txt$/i);
